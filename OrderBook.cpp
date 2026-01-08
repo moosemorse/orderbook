7e3a99844a467c730cac4e3831828bfdec79191e
moosemorse <a.ivanov6174@gmail.com>
2026-01-08 08:11:56 +0000

    feat: OrderBookLevelInfos class has APIs to get different levels of orderbook (bids+asks).

--- OrderBook.cpp
+++ OrderBook.cpp
@@ -37,6 +37,38 @@ using Price = std::int32_t;
 using Quantity = std::uint32_t;
 using OrderId = std::uint64_t;
 
+/* LevelInfo useful for public APIs to interact with state of orderbook. */
+struct LevelInfo
+{
+  Price price_;
+  Quantity quantity_;
+};
+
+using LevelInfos = std::vector<LevelInfo>;
+
+// we want to encapsulate level infos
+class OrderBookLevelInfos
+{
+public:
+  OrderBookLevelInfos(const LevelInfos &bids, const LevelInfos &asks)
+      : bids_{bids}, asks_{asks}
+  {
+  }
+
+  const LevelInfos &GetBids() const
+  {
+    return bids_;
+  }
+  const LevelInfos &GetAsks() const
+  {
+    return asks_;
+  }
+
+private:
+  LevelInfos bids_;
+  LevelInfos asks_;
+};
+
 int main()
 {
   return 0;
