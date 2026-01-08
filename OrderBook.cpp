8dfb1d44265dade07ab7c323286187900bdae540
moosemorse <a.ivanov6174@gmail.com>
2026-01-08 09:05:09 +0000

    feat: Order class implemented for objects in orderbook.

--- OrderBook.cpp
+++ OrderBook.cpp
@@ -69,6 +69,73 @@ private:
   LevelInfos asks_;
 };
 
+/* Now we need to describe the things we add to an order book which are order objects. */
+class Order
+{
+public:
+  // note about quantity: when we talk about an order we really have 3 quantities
+  // initial quantity, remaining quantity, and amount filled
+  Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
+      : orderType_{orderType}, orderId_{orderId}, side_{side}, price_{price}, initialQuantity_{quantity}, remainingQuantity_{quantity}
+  {
+  }
+
+  // note: const just before func body means that the function is part of a class
+  // and cant change any members of that class AND only const objects can call these functions
+  OrderId GetOrderId() const
+  {
+    return orderId_;
+  }
+
+  Side GetSide() const
+  {
+    return side_;
+  }
+
+  Price GetPrice() const
+  {
+    return price_;
+  }
+
+  OrderType GetOrderType() const
+  {
+    return orderType_;
+  }
+
+  // might not be important but mentioned in some documentation
+  // to be honest, we care more about remaining quantity
+  Quantity GetInitialQuantity() const
+  {
+    return initialQuantity_;
+  }
+
+  Quantity GetRemainingQuantity() const
+  {
+    return remainingQuantity_;
+  }
+
+  Quantity GetFilledQuantity() const
+  {
+    return GetInitialQuantity() - GetRemainingQuantity();
+  }
+
+  void Fill(Quantity quantity)
+  {
+    if (quantity > GetRemainingQuantity()) // note, std::format works by {} being replaced by *args
+      throw std::logic_error(std::format("Order ({}) cannot be filled for more than its remaining quantity.", GetOrderId()));
+
+    remainingQuantity_ -= quantity;
+  }
+
+private:
+  OrderType orderType_;
+  OrderId orderId_;
+  Side side_;
+  Price price_;
+  Quantity initialQuantity_;
+  Quantity remainingQuantity_;
+};
+
 int main()
 {
   return 0;
