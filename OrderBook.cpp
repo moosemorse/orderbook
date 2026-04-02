92f792d2608aab0c6f1a806e0be3e9c002699d6e
moosemorse <a.ivanov6174@gmail.com>
2026-04-02 11:16:52 +0100

    feat(trade): implement trade api for asks and bids

--- OrderBook.cpp
+++ OrderBook.cpp
@@ -172,6 +172,33 @@ private:
   Quantity quantity_;
 };
 
+struct TradeInfo
+{
+  OrderId orderId_;
+  Price price_;
+  Quantity quantity_;
+};
+
+// an order matched is the aggregation of two trade info objects (for ask and bid)
+class Trade
+{
+public:
+  Trade(const TradeInfo& bidTrade, const TradeInfo& askTrade)
+      : bidTrade_{bidTrade}, askTrade_{askTrade}
+  {
+  }
+
+  const TradeInfo &GetBidTrade() const { return bidTrade_; }
+  const TradeInfo &GetAskTrade() const
+  {
+    return askTrade_;
+  }
+
+private:
+  TradeInfo bidTrade_;
+  TradeInfo askTrade_;
+};
+
 int
 main()
 {
