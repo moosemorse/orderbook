ca6ae925222aa49d1ba53e41519fadeccf0064c3
moosemorse <a.ivanov6174@gmail.com>
2026-04-02 11:12:49 +0100

    feat(orders): implement modify order api

--- OrderBook.cpp
+++ OrderBook.cpp
@@ -148,7 +148,32 @@ using OrderPointers = std::list<OrderPointer>;
 // add and cancel are simple, modify requires the ability to modify different
 // attributes like remaining price or side (side isn't necessary)
 
-int main()
+class OrderModify
+{
+public:
+  OrderModify(OrderId orderId, Side side, Price price, Quantity quantity)
+      : orderId_{orderId}, price_{price}, side_{side}, quantity_{quantity} {}
+
+  OrderId GetOrderId() const { return orderId_; }
+  Price GetPrice() const { return price_; }
+  Side GetSide() const { return side_; }
+  Quantity GetQuantity() const { return quantity_; }
+
+  // current: only can modify good till cancel, but can be extended
+  OrderPointer ToOrderPointer(OrderType type) const
+  {
+    return std::make_shared<Order>(type, GetOrderId(), GetSide(), GetPrice(), GetQuantity());
+  }
+
+private:
+  OrderId orderId_;
+  Price price_;
+  Side side_;
+  Quantity quantity_;
+};
+
+int
+main()
 {
   return 0;
 }
