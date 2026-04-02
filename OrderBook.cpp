8dd2a0dbef58adf2868d6980fe6edcf5d155d7eb
moosemorse <a.ivanov6174@gmail.com>
2026-04-02 12:49:30 +0100

    feat(main): simple test to see if adding order to orderbook increments size

--- OrderBook.cpp
+++ OrderBook.cpp
@@ -427,5 +427,12 @@ public:
 
 int main()
 {
+  // simple main suite
+  Orderbook orderbook;
+  const OrderId orderId {1};
+  orderbook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, orderId, Side::Buy, 100, 10));
+  std::cout << orderbook.Size() << std::endl; // 1
+  orderbook.CancelOrder(orderId);
+  std::cout << orderbook.Size() << std::endl; // 0
   return 0;
 }
